0f8796b640c89cf72f0e388a3d8fc8a2d160ec7e
ornse01 <ornse01@users.osdn.me>
2016-12-12 17:49:11 +0900

    fix undefined symbol.

--- rlib/src/lib.rs
+++ rlib/src/lib.rs
@@ -13,14 +13,14 @@ pub extern fn minus_one(x: i32) -> i32 {
 
 extern {
     fn link_confirmation(value: i32) -> i32;
-    fn chg_pri(id: i32, pri: i32, opt: i32) -> i32;
+    fn b_chg_pri(id: i32, pri: i32, opt: i32) -> i32;
 }
 
 #[no_mangle]
 pub extern fn sample_call(value: i32) -> i32 {
     unsafe {
         link_confirmation(value);
-        let err = chg_pri(0, 0, 0);
+        let err = b_chg_pri(0, 0, 0);
         err
     }
 }
