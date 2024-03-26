2618529cc6eaac7a6a8dfd89830d98fc2a74a1de
ornse01 <ornse01@users.osdn.me>
2024-03-26 18:00:38 +0900

    add API

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate alloc;
 use core::alloc::{GlobalAlloc, Layout};
 use core::ffi::{c_char, c_size_t};
 use core::panic::PanicInfo;
+use core::result::Result;
 use core::str;
 
 use alloc::ffi::CString;
@@ -16,12 +17,49 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+pub type B = i8;
+pub type H = i16;
+pub type W = i32;
+pub type UB = u8;
+pub type UH = u16;
+pub type UW = u32;
+
+pub type TC = UH;
+
+pub type ERR = W;
+
+const TNULL: TC = 0;
+
+const TK_A: TC = 0x2341;
+const TK_B: TC = TK_A + 1;
+const TK_T: TC = TK_A + 19;
+const TK_R: TC = TK_A + 17;
+const TK_O: TC = TK_A + 14;
+const TK_N: TC = TK_A + 13;
+
 extern "C" {
     fn b_chg_pri(id: i32, pri: i32, opt: i32) -> i32;
+    fn b_ext_prc(code: W);
+    fn b_pdsp_msg(msg: *const TC) -> W;
     fn printf(format: *const c_char, value: i32) -> i32;
 
     fn malloc(size: c_size_t) -> *mut u8;
     fn free(p: *mut u8);
+}
+
+pub fn ext_prc(code: W) {
+    unsafe { b_ext_prc(code) }
+}
+
+pub fn pdsp_msg(msg: &[TC]) -> Result<W, W> {
+    let err;
+    let msg_ptr = msg.as_ptr();
+    unsafe { err = b_pdsp_msg(msg_ptr) }
+    if err >= 0 {
+        Ok(err)
+    } else {
+        Err(err)
+    }
 }
 
 struct BTRONAllocator {}
