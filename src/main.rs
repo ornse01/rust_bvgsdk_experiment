c38ed8d12dcb5d777fa55cf75f6728b98a23826f
ornse01 <ornse01@users.osdn.me>
2024-03-26 18:10:15 +0900

    replace to sample code

--- src/main.rs
+++ src/main.rs
@@ -52,18 +52,6 @@ const TK_t: TC = TK_a + 19;
 #[global_allocator]
 static ALLOCATOR: Allocator = Allocator {};
 
-fn plus_one(x: i32) -> i32 {
-    x + 1
-}
-
-fn minus_one(x: i32) -> i32 {
-    x - 1
-}
-
-fn sample_call(_value: i32) -> i32 {
-    return chg_pri(0, 0, 0).unwrap_err();
-}
-
 #[repr(C)]
 pub struct MESSAGE {
     msg_type: i32,
@@ -71,15 +59,10 @@ pub struct MESSAGE {
 }
 
 #[no_mangle]
-pub extern "C" fn MAIN(target: *mut MESSAGE) -> i32 {
-    unsafe {
-        print!("msg_type: {}\n", (*target).msg_type);
-        print!("msg_size: {}\n", (*target).msg_size);
-    }
-
-    println!("test: {}", plus_one(2));
-    println!("test: {}", minus_one(2));
-    println!("test: {}", sample_call(2));
+pub extern "C" fn MAIN(_target: *mut MESSAGE) -> i32 {
+    let mes01: [TC; 6] = [TK_B, TK_T, TK_R, TK_O, TK_N, TNULL];
+    let _ = pdsp_msg(&mes01);
+    ext_prc(0);
 
     return 0;
 }
